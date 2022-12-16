fe93486ccb7cbfea15b8e06bf79eee21cae23f3c
Eniola Olajide <olajideenioa75@gmail.com>
2022-12-15 18:41:24 -0800

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_line - > printing line
+ * print_line -> printing line
  * @n: integer params
  */
 void print_line(int n)
 {
 	int x;
 
-	if (n<= 0)
-		_putchar('\n);
+	if (n <= 0)
+		_putchar('\n');
 	else
 	{
 		for (x = 0; x < n; x++)
