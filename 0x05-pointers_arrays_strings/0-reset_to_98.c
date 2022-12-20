a23f5dbee57f995575528626aea20216d64d93b4
Eniola Olajide <olajideenioa75@gmail.com>
2022-12-20 01:53:52 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,14 @@
-#include "eniola.h"
-
+#include "main.h"
 
 /**
- * reset_to_98n - take a pointer to an int as parameter and updates
- * the value it points to 98
- * @n: point to an int
- * Return: Nothing
+ * reset_to_98 - function that takes a pointer to an int as parameter and
+ * updates the value it points to to 98.
+ *
+ * @n: This is the entry
+ *
  */
 
-void reset_to 98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
