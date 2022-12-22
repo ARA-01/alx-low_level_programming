64c8f6c9bd036a743a73a5f1ce5d5a412a4345d8
Eniola Olajide <olajideenioa75@gmail.com>
2022-12-22 02:03:32 -0800

    task 4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,14 @@
 
 /**
  * _puts - prints a string, followed by a new line, to stdout.
- *
+
  * @str: string to be printed
  */
 void _puts(char *str)
 {
-	while (*st != '\0')
+	while (*str)
 	{
-		_putschar(*str++);
-	}
+		_putschar(*str);
+		str++;
 		_putschar('\n');
 }
