80a290caee08a6c6150a1ee6f81c5e1308f923e9
Eniola Olajide <olajideenioa75@gmail.com>
2022-12-23 21:04:18 -0800

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
  * int protototypes()
  * *char protototypes()
  */
-
+int _putchar(char c);
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
@@ -14,9 +14,10 @@ int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);	
-char *string_toupper(char *);
+char *string_toupper(char *a);
 char *cap_string(char *);
-char *leet(char *);char *rot13(char *);
+char *leet(char *a);
+char *rot13(char *a);
 char*infinie_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
