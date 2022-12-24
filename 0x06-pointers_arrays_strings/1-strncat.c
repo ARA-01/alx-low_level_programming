fc4f66d275f1e0b9e02b8b13438badf3d038cd8e
Eniola Olajide <olajideenioa75@gmail.com>
2022-12-23 21:39:48 -0800

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
  * _strncat - jois two strings with n bytes
  * @dest: copy to
  * @src: copy from
- * @N: number of char to be copied
+ * @n: number of char to be copied
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
@@ -13,7 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 	int j;
 
 	i = 0;
-	while (dest[i] !='\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
