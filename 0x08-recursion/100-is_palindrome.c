4b6e7dea2571de8aef7a163016033437c9d987c1
Eniola Olajide <olajideenioa75@gmail.com>
2023-01-04 12:44:03 -0800

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,15 +16,15 @@ int _length(char *s)
  * @i: is the index
  * @lg: is the length of the string
  * @s: is the string
- * Return 1 if is palindrome or 0 if not
- **/
+ * Return: 1 if is palindrome or 0 if not
+ */
 int checkp(int i, int lg, char *s)
 {
 	if (lg > 0)
 	{
 		if (s[i] == s[lg])
 		{
-			return (checkp(i - 1, lg - 1, s));
+			return (checkp(i + 1, lg - 1, s));
 		}
 		else if (s[i] != s[lg])
 		{
