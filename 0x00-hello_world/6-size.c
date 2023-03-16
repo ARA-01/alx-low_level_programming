16368b1f74193dfdff68466a9c539c65388639c5
ARA-01 <olajideeniola75@gmail.com>
2023-03-16 16:52:53 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,15 @@
 /**
  * main - this is the main function
  * *
- * Return: Always 0.
+ * Return: Always zero.
  */
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));	
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("SIze of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
+	printf("Size of a float: %lu bytes\n", sizeof(float));
 	return (0);
 }
