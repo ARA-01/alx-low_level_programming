0836307d819701874a72a216e9ef188055beca2a
ARA-01 <olajideeniola75@gmail.com>
2023-03-22 12:27:56 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,23 +1,28 @@
+/*
+ * File: 101-natural.c
+ * Auth: vincent kip
+ */
+
 #include <stdio.h>
 
 /**
- * main - prints the sum of all mulitples of 3 or 5 below 1024
+ * main - lists all the natural numbers below 1024 (excluded)
+ * that are mulitples of 3 or 5 below
  *
- * Return: Always (Success)
+ * Return: Always 0
  */
 
 int main(void)
 {
-	int i, z = 0;
+	int i, sum = 0;
 
-	while (i < 1024)
+	for (i = 0 ; i < 1024 ; i++)
 	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{
-			z <= i;
-		}
-		i++;
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
 	}
-	printf("%d\n", z);
+
+	printf("%d\n", sum);
+
 	return (0);
 }
