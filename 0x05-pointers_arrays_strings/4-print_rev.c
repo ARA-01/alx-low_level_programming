ea0c29cf5ba3a8780f92aab810050c5bab214793
ARA-01 <olajideeniola75@gmail.com>
2023-03-27 15:17:00 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
  * print_rev - prints string in reverse
- * @s: functio parameter
+ * @s: function parameter
  *
  * Return: 0
  */
@@ -12,9 +12,9 @@ void print_rev(char *s)
 	int i;
 	int count = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	for (i = count; i >= 0; i--)
+	for (i = count ; i >= 0 ; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
