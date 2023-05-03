46a59af889d22ed034cf609d08169440ff1ef9ae
ARA-01 <olajideeeniola75@gmail.com>
2023-05-03 12:28:27 -0700

    C - More Singly Linked List

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,18 +9,18 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev_node = NULL;
-	listint_t *next_node = NULL;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
 	while (*head)
 	{
-		next_node = (*head)->next_node;
-		(*head)->next_node = prev_node;
-		prev_node = *head;
-		*head = next_node;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 
-	*head = prev_node;
+	*head = prev;
 
 	return (*head);
 }
