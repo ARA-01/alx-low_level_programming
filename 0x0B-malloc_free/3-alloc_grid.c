9a875979886687a34f1c43bba2942dda90e5eb34
ARA-01 <olajideeniola75@gmail.com>
2023-06-14 07:42:34 -0700

    Task 3: Function that points to a 2D array of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - function that returns a pointer to a 2-D array of integers.
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: a pointer to a 2 dimensional array of integers.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array, p, q;
+	int len = width * height;
+
+	if (len <= 0)
+		return (NULL);
+
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; p++)
+	{
+		array[p] = (int *)malloc(sizeof(int) * width);
+		if (array[p] == NULL)
+		{
+			for (p--; p >= 0; p--)
+				free(array[p]);
+			free(array);
+			return (NULL);
+		}
+	}
+	for (p = 0; p < height; p++)
+		for (q = 0; q < width; q++)
+			array[p][q] = 0;
+	return (array);
+}
+
